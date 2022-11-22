6b51b399aff4879bfd01bcbf87ab2bff340a4d85
Reyniel Mark <58727783+reyniel26@users.noreply.github.com>
2022-11-22 14:53:34 +0800

    Updated test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,12 +1,23 @@
+"""
+Test
+"""
 import unittest
 
 from src.example_package_reyniel26.example import add_one
 
 
 class TestIncrement(unittest.TestCase):
-
+    """
+    Test
+    """
     def test_add_to_2(self):
+        """
+        Test
+        """
         self.assertEqual(add_one(2), 3, "Should be 3")
 
     def test_add_to_20(self):
+        """
+        Test
+        """
         self.assertEqual(add_one(20), 21, "Should be 21")
